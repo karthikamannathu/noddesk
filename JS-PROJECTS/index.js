99111b6377e72c768cd9c81e71b74bd7259d350b
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-02 12:21:31 +0530

    reduce fun is wrk.

--- JS-PROJECTS/index.js
+++ JS-PROJECTS/index.js
@@ -1,4 +1,9 @@
-const numbers = [1,2,3,4,5,7];
-const evenNumber = numbers.filter(num => num % 2 == 0);
-console.log(evenNumber);
-console.log(numbers);
+const prices = [10.50, 5.25, 20.00, 3.75];
+
+const total = prices.reduce((accumlator,currentValue) => {
+    console.log(`Accumlator:${accumlator},current value: ${currentValue}`)
+    return currentValue + accumlator;
+
+},0);
+
+console.log(`final total :${total}`);
