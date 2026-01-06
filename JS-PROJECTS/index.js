6a9ae771a012634c21a6a7475858546d2c319501
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-06 22:53:05 +0530

    Promise creations

--- JS-PROJECTS/index.js
+++ JS-PROJECTS/index.js
@@ -1,9 +1,18 @@
-const prices = [10.50, 5.25, 20.00, 3.75];
-
-const total = prices.reduce((accumlator,currentValue) => {
-    console.log(`Accumlator:${accumlator},current value: ${currentValue}`)
-    return currentValue + accumlator;
-
-},0);
-
-console.log(`final total :${total}`);
+const fetchData = new Promise((resolve, reject) =>{
+ console.log('Executor function stared... (Fetach)')
+ 
+ 
+ setTimeout(() => {
+    const success = true;
+    if (success) {
+        const data = { id: 1,message:'Hello Promise!'};
+        console.log('Data fetched. Resolving promise!')
+       resolve(data);
+    } else {
+        const error = new Error('culoud not fetch data!');
+        console.log('Error fetching Rejecting promise.')
+        reject(error);
+        
+    }
+ },2000);
+})
