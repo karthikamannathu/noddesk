423d8240bd636ac05c24e577393ad35182bec917
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-07 22:50:12 +0530

    save note localstorage and use setItem(),getItem(),removeItem()

--- JS-PROJECTS/index.js
+++ JS-PROJECTS/index.js
@@ -1,23 +1,3 @@
-async function fetchDataSafe(params) {
-    try {
-    console.log('Fetching all user data ...')
 
-    const user = await fetchUser('user123');
-    console.log('Got user :',user);
-    
-    const posts = await fetchPosts(user.id);
-    console.log('Got post:',posts);
 
-    const comments = await fetchComments(posts[0].id);
-    console.log('Got user :',comments);
-    
-    return comments;
-        
-    } catch (error) {
-       console.error('An error occured:',error); 
-    } finally{
-        console.log("All operations are complete.");
-    }
-}
 
-fetchDataSafe();
