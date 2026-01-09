6eff7d3662eb72a7b9c404f02617f1fc0b1bf851
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-09 12:54:31 +0530

    creating custome error

--- JS-PROJECTS/index.js
+++ JS-PROJECTS/index.js
@@ -1,3 +1,19 @@
+ function calculateSqureRoot(number) {
+if (typeof number !== 'number') {
+    throw new TypeError('Invalued input: a number was excuted');
+}
+if (number < 0) {
+    throw new Error( "the negative value was excuted");  
+}
+return Math.sqrt(number);
+}
 
 
-
+try {
+    const result1 = calculateSqureRoot('e');
+    console.log(result1,"Result 1");
+    const result2 = calculateSqureRoot(-9);
+    console.log(result2,"Result 2")
+} catch (error) {
+    console.error('caught a custom error:',error.message);
+}
